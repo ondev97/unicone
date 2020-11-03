7f7f5d7901eb9ea172b0ce3a4f9badb5a7f069ce
Navanjane <nkindelpitiya@gmail.com>
2020-11-03 20:40:21 +0530

    settings.py updated

--- lms/settings.py
+++ lms/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
