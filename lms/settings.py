b292bd23ca751b8db12109dddd39c5e7b7b3a597
Navanjane <nkindelpitiya@gmail.com>
2020-11-13 19:54:27 +0530

    media url added

--- lms/settings.py
+++ lms/settings.py
@@ -145,10 +145,8 @@ REST_AUTH_SERIALIZERS = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
