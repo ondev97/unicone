db7b613bd1ae5a75649e6ee4205a995e8b946919
Navanjane <nkindelpitiya@gmail.com>
2020-11-17 13:39:32 +0530

    Permissions added to django admin

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,TeacherProfile,StudentProfile
+from .models import User,TeacherProfile,StudentProfile,GroupAdminForm,Group
 from django.contrib.auth.admin import UserAdmin
 
 #Register your models here.
@@ -16,5 +16,18 @@ class AccountAdmin(UserAdmin):
 admin.site.register(User,AccountAdmin)
 admin.site.register(TeacherProfile)
 admin.site.register(StudentProfile)
+
+# Unregister the original Group admin.
+admin.site.unregister(Group)
+
+# Create a new Group admin.
+class GroupAdmin(admin.ModelAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+
+# Register the new Group ModelAdmin.
+admin.site.register(Group, GroupAdmin)
 
 
