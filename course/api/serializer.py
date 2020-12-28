54d9206866b6cdbe566b14c23a10406a5e180fdf
Navanjane <nkindelpitiya@gmail.com>
2020-12-28 23:24:29 +0530

    short description added

--- course/api/serializer.py
+++ course/api/serializer.py
@@ -127,7 +127,7 @@ class SubjectViewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Subject
-        fields = ['id','subject_name','subject_cover','subject_type','class_type','author','short_description']
+        fields = ['id','subject_name','subject_cover','subject_type','class_type','author','short_description','created_at']
 
         depth = 2
 
