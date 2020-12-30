6d4b102bb4a75a7fcc8d32b0f42210b2f84a45ec
Navanjane <nkindelpitiya@gmail.com>
2020-12-30 23:18:21 +0530

    mycourses view added

--- course/api/views.py
+++ course/api/views.py
@@ -233,7 +233,7 @@ def EnrollCourse(request,pk,upk):
 # Listing Enrolled Courses
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def MyCourses(request,upk):
+def MyCourses(request):
     student = StudentProfile.objects.get(user_id=request.user.id)
     courses_enrolled = Enrollment.objects.filter(student=student).order_by('-id')
     serializer = MycoursesSerializer(courses_enrolled,many=True)
