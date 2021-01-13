71c0da4298c585d20e611d35a7fd4456930614a8
Navanjane <nkindelpitiya@gmail.com>
2021-01-13 23:05:42 +0530

    index subject limited

--- course/api/views.py
+++ course/api/views.py
@@ -585,7 +585,7 @@ def LatestSubjects(request):
 
 @api_view(['GET'])
 def SubjectListIndex(request):
-    subjects = Subject.objects.all()
+    subjects = Subject.objects.all().order_by('-id')[:5]
     serializer = SubjectSerializer(subjects, many=True)
     for i in range(len(serializer.data)):
         serializer.data[i]['author']['user'].pop('password')
