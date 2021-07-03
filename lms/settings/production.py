eb490eeca19109e9df03fb737e9f8298c812372b
navanjane <nkindelpitiya@gmail.com>
2021-07-03 11:56:44 +0530

    Spaces config added

--- lms/settings/production.py
+++ lms/settings/production.py
@@ -16,3 +16,20 @@ DATABASES = {
 INSTALLED_APPS += [
     'storages',
 ]
+
+
+AWS_ACCESS_KEY_ID = 'DBDCWQUAQQIZDJ3PH7HK'
+AWS_SECRET_ACCESS_KEY = 'qbDBtv/Fb5pp4RJncImFM/mpPIzYCPgGRxnxuM7iphM'
+AWS_STORAGE_BUCKET_NAME = 'unicone-data'
+AWS_S3_ENDPOINT_URL = 'https://sgp1.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'Data'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
