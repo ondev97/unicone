4aa82bd7be22270b30651e2b94588e7e54ee9e2f
navanjane <nkindelpitiya@gmail.com>
2021-07-03 11:25:04 +0530

    Settings updated to production

--- lms/settings/production.py
+++ lms/settings/production.py
@@ -1,1 +1,14 @@
 from .base import *
+
+ALLOWED_HOSTS = ['128.199.145.231']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unicone',
+        'USER': 'unicone',
+        'PASSWORD': 'unicone',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
