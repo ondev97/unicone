e9aa17d6802458b0939c7aa29aaa4c0e7d128d06
navanjane <nkindelpitiya@gmail.com>
2021-07-03 11:27:17 +0530

    Settings updated to production

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings.prodcution')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
